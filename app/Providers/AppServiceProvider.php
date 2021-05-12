ac4f15396f047c1238388e90c15e6ab97b1ba3d8
mafeijam <mafei27@hotmail.com>
2021-05-12 17:11:13 +0000

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $host = request()->getHost();
+        dd(request());
+        // request()->header('cf-connecting-ip');
+        if (str_contains($host, 'jamwong.me')) {
+            config([
+                'app.url' => 'https://'.$host,
+                'session.domain' => $host,
+                'stateful.stateful' => $host,
+            ]);
+        }
     }
 }
